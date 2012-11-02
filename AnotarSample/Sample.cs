4c809feefaf5f03305952e6a6d62b7a8d35f90ad
Simon Cropp <simon.cropp@gmail.com>
2012-11-02 20:28:29 +1100

    add anotar sample

---
+++ AnotarSample/Sample.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Anotar;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+using NUnit.Framework;
+
+[TestFixture]
+public class Sample
+{
+    [Test]
+    public void Run()
+    {
+        var getLog = SetupNLog();
+        MyMethod();
+
+        Assert.AreEqual("Method: MyMethod. Line: ~24. TheMessage", getLog());
+    }
+
+    static void MyMethod()
+    {
+        Log.Debug("TheMessage");
+    }
+
+    Func<string> SetupNLog()
+    {
+        var target = new MemoryTarget
+                         {
+                             Layout = "${message}"
+                         };
+        var config = new LoggingConfiguration();
+        config.AddTarget("trace", target);
+
+        config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, target));
+
+        LogManager.Configuration = config;
+
+        return ()=> target.Logs.First();
+    }
+
+}
