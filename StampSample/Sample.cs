3a6f899b84cc59e98f4e1fc67a116cf86ddd9500
Simon Cropp <simon.cropp@gmail.com>
2012-11-02 23:30:25 +1100

    add stamp sample

---
+++ StampSample/Sample.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+
+[TestFixture]
+public class Sample
+{
+    [Test]
+    public void Run()
+    {
+        var infoAttribute = (AssemblyInformationalVersionAttribute)GetType().Assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false).First();
+        Debug.WriteLine(infoAttribute.InformationalVersion);
+        Assert.IsTrue(infoAttribute.InformationalVersion.Contains("/"));
+
+    }
+}
