3bc30fa990fd4ac020ae6583fbc2e5681de82597
Simon Cropp <simon.cropp@gmail.com>
2012-11-24 17:32:17 +1100

    add immutable sample

--- FielderSample/Sample.cs
+++ FielderSample/Sample.cs
@@ -6,8 +6,12 @@ public class Sample
     [Test]
     public void Run()
     {
-        Assert.IsNotNull(GetType().GetProperty("MemberToConvert"));
+        Assert.IsNotNull(typeof(Target).GetProperty("MemberToConvert"));
     }
+}
+
+public class Target
+{
 
     public string MemberToConvert;
 }
---
+++ ImmutableSample/Sample.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class Sample
+{
+    [Test]
+    public void Run()
+    {
+        Assert.IsTrue(typeof(Target).GetField("Field").IsInitOnly);
+    }
+
+}
+
+[Immutable]
+public class Target
+{
+    public string Field;
+
+    //uncomment this and you will get a build error
+    //void BadMethodWithFieldWrite()
+    //{
+    //    Field = "sdf";
+    //}
+}
