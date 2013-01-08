8e082f3ab358268c5566e92df1a48501a2147efb
Simon Cropp <simon.cropp@gmail.com>
2013-01-09 01:53:35 +1100

    add null guard sample

---
+++ NullGuardSample/Sample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+[TestFixture]
+public class Sample
+{
+    [Test]
+    public void Run()
+    {
+        var targetClass = new TargetClass();
+        Assert.Throws<ArgumentNullException>(() => targetClass.Method(null));
+    }
+}
+public class TargetClass
+{
+    public void Method(string param)
+    {
+        
+    }
+}
