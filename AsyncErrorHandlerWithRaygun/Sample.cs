64685ad8561818d610f7113acd90fa9b80d0d14f
Simon Cropp <simon.cropp@gmail.com>
2013-01-25 22:11:20 +1100

    add raygun sample

---
+++ AsyncErrorHandlerWithRaygun/Sample.cs
@@ -0,0 +1,40 @@
+#if (!MONO)
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Mindscape.Raygun4Net;
+using NUnit.Framework;
+
+[TestFixture]
+public class Sample
+{
+    [Test]
+    public async void Run()
+    {
+        var instance = new Target();
+        instance.MethodWithThrow();
+        //run and have a look at your Raygun dashboard
+        Thread.Sleep(10);
+    }
+}
+
+public class Target
+{
+    public async Task MethodWithThrow()
+    {
+        await Task.Delay(1);
+        throw new Exception("MyExcpetion");
+    }
+}
+
+public static class AsyncErrorHandler
+{
+    public static void HandleException(Exception exception)
+    {
+        var apiKey = "";//TODO: set your Raygun ApiKey
+        var raygunClient = new RaygunClient(apiKey);
+        raygunClient.Send(exception);
+    }
+}
+
+#endif
