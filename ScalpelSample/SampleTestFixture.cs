e7550a26016897702d2f4a1d0278bf13fa539518
Simon Cropp <simon.cropp@gmail.com>
2013-02-24 20:11:11 +1100

    add scalpel sample     } Scalpel sample

---
+++ ScalpelSample/SampleTestFixture.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+//Have a look at the resultant assembly in release mode. 
+//This class and the nunit reference will have been removed.
+[TestFixture]
+public class SampleTestFixture
+{
+    [Test]
+    public void Test1()
+    {
+    }
+}
