edfda7ab936227acb26ccdd66a5ee4aeb4afee73
Simon Cropp <simon.cropp@gmail.com>
2013-05-06 22:17:54 +1000

    add resourcer sample

---
+++ ResourcerSample/Sample.cs
@@ -0,0 +1,14 @@
+﻿
+using NUnit.Framework;
+using Resourcer;
+
+[TestFixture]
+public class Sample
+{
+    [Test]
+    public void Run()
+    {
+        var fromResource = Resource.AsString("SampleResource.txt");
+        Assert.AreEqual("Hello", fromResource);
+    }
+}
