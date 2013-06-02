aea871f8f611e9c4c77dcfe14ee1bcb0c4f3256a
Simon Cropp <simon.cropp@gmail.com>
2013-06-02 22:43:16 +1000

    add tostring sample

---
+++ ToStringSample/Sample.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using NUnit.Framework;
+
+[TestFixture]
+public class ToStringSample
+{
+    [Test]
+    public void Run()
+    {
+        var target = new Person
+                     {
+                         GivenNames = "John",
+                         FamilyName = "Smith"
+
+                     };
+        Debug.WriteLine(target.ToString());
+        Assert.AreEqual("{T: Person, GivenNames: \"John\", FamilyName: \"Smith\"}",target.ToString());
+    }
+}
+
+[ToString]
+class Person
+{
+    public string GivenNames { get; set; }
+    public string FamilyName { get; set; }
+
+      [IgnoerDuringToString]
+    public string FullName
+    {
+        get
+        {
+            return string.Format("{0} {1}", GivenNames, FamilyName);
+        }
+    }
+}
