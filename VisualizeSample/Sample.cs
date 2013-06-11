deb99fa747ec9c4db9da915b1b18eac790da4f59
Simon Cropp <simon.cropp@gmail.com>
2013-06-11 23:00:12 +1000

    add visualize sample

---
+++ VisualizeSample/Sample.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using NUnit.Framework;
+
+[TestFixture]
+public class VisualizeSample
+{
+    [Test]
+    public void Run()
+    {
+        var person = new Person
+                     {
+                         FamilyName = "Smith", 
+                         GivenNames = "John James"
+                     };
+        //Set a breakpoint here and look at person int he debugger
+        Debug.WriteLine(person);
+    }
+
+}
+public class Person 
+{
+    public string GivenNames { get; set; }
+    public string FamilyName { get; set; }
+
+    public string FullName
+    {
+        get
+        {
+            return string.Format("{0} {1}", GivenNames, FamilyName);
+        }
+    }
+}
