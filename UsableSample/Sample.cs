bb4a7d32cffd4d3375c635845a475b2a7d10519b
Simon Cropp <simon.cropp@gmail.com>
2013-07-07 14:51:06 +1000

    add Usable sample

---
+++ UsableSample/Sample.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+
+[TestFixture]
+public class UsableSample
+{
+    [Test]
+    public void Run()
+    {
+        MethodWithDisposableVariable();
+        Assert.IsTrue(Disposable.Disposed);
+    }
+    public static void MethodWithDisposableVariable()
+    {
+        var disposable = new Disposable();
+        disposable.Method();
+    }
+}
+
+
+public class Disposable : IDisposable
+{
+    public void Dispose()
+    {
+        Disposed = true;
+    }
+
+    public void Method()
+    {
+        
+    }
+
+    public static bool Disposed;
+}
