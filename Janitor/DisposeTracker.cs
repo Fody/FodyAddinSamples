8125fb22517620d8467e89f82b91afcfde3fa76e
Simon Cropp <simon.cropp@gmail.com>
2013-07-14 18:22:11 +1000

    add janitor sample

---
+++ Janitor/DisposeTracker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// A IDisposable class we will use to verify that fields in the sample have been disposed
+/// </summary>
+[Janitor.SkipWeaving]
+public class DisposeTracker : IDisposable
+{
+    public static bool HasDisposedBeenCalled;
+
+    public void Dispose()
+    {
+        HasDisposedBeenCalled = true;
+    }
+}
---
+++ Janitor/Sample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+
+[TestFixture]
+public class JanitorSample
+{
+    [Test]
+    public void Run()
+    {
+        var disposable = new Disposable();
+        disposable.Dispose();
+        Assert.IsTrue(DisposeTracker.HasDisposedBeenCalled);
+    }
+
+    public class Disposable : IDisposable
+    {
+        DisposeTracker disposeTracker;
+
+        public Disposable()
+        {
+            disposeTracker = new DisposeTracker();
+        }
+
+        public void Dispose()
+        {
+            //must be empty
+        }
+    }
+
+}
