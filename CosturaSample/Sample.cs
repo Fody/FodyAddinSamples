0eb36065d05c6483a0cbe67648fbcabc903f1e78
Simon Cropp <simon.cropp@gmail.com>
2013-07-14 20:01:47 +1000

    try fix build

--- CosturaSample/Sample.cs
+++ CosturaSample/Sample.cs
@@ -1,12 +1,22 @@
 ﻿using System.Diagnostics;
+using System.IO;
 using NUnit.Framework;
 
 [TestFixture]
 public class CosturaSample
 {
+
+    [TestFixtureSetUp]
+    public void Setup()
+    {
+
+        var path = Path.Combine(AssemblyLocation.CurrentDirectory(), "CosturaAssemblyToReference.dll");
+        File.Delete(path);
+    }
     [Test]
     public void Run()
     {
+
         //Note that this will work even though CosturaAssemblyToReference.dll does not exists in the execution directory
         Debug.WriteLine(ClassInReferenceAssembly.SayHello());
     }
