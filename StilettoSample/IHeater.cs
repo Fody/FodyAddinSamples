d9c41b89847bcbe0537f0debc43dbb9700553210
Ben Bader <ben.bader@rd.io>
2013-07-16 18:16:03 -0700

    Added a working sample for the Stiletto weaver.

---
+++ StilettoSample/IHeater.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StilettoSample
+{
+    public interface IHeater
+    {
+        bool IsHot { get; }
+        void On();
+        void Off();
+    }
+}
