868729bd6bff645d11929edd8047616e83eb3c2e
Ben Bader <ben.bader@rd.io>
2013-07-16 18:16:03 -0700

    Added a working sample for the Stiletto weaver.

---
+++ StilettoSample/DripCoffeeModule.cs
@@ -0,0 +1,16 @@
+﻿using Stiletto;
+
+namespace StilettoSample
+{
+    [Module(
+        Injects = new[] { typeof(Sample) },
+        IncludedModules = new[] { typeof(PumpModule) })]
+    public class DripCoffeeModule
+    {
+        [Provides, Singleton]
+        public IHeater GetHeater()
+        {
+            return new ElectricHeater();
+        }
+    }
+}
---
+++ StilettoSample/PumpModule.cs
@@ -0,0 +1,14 @@
+﻿using Stiletto;
+
+namespace StilettoSample
+{
+    [Module(IsComplete = false)]
+    public class PumpModule
+    {
+        [Provides]
+        public IPump GetPump(Thermosiphon thermosiphon)
+        {
+            return thermosiphon;
+        }
+    }
+}
