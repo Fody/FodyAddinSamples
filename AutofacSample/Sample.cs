83c4512769002e8e6d0eac5f157f37be6bece21b
Simon Cropp <simon.cropp@gmail.com>
2013-07-21 12:57:02 +1000

    simplify

--- AutofacSample/Sample.cs
+++ AutofacSample/Sample.cs
@@ -2,64 +2,61 @@
 using Fody.DependencyInjection;
 using NUnit.Framework;
 
-namespace AutofacFodySample
+[TestFixture]
+public class AutofacSample
 {
-    [TestFixture]
-    public class Sample
+    [TestFixtureSetUp]
+    public void Setup()
     {
-        [TestFixtureSetUp]
-        public void Setup()
-        {
-            var builder = new ContainerBuilder();
-            builder.RegisterType<Service>().As<IService>()
-                .WithParameter("injectedValue", 5); 
+        var builder = new ContainerBuilder();
+        builder.RegisterType<Service>().As<IService>()
+            .WithParameter("injectedValue", 5); 
 
-            ConfigurableInjection.InitializeContainer(builder.Build());
-        }
-
-        [Test]
-        public void AutofacServiceIsInjected()
-        {
-            var entity = new Entity(5);
-
-            Assert.IsNotNull(entity.Service);
-            Assert.IsInstanceOf(typeof(Service), entity.Service);
-
-            Assert.AreEqual(25, entity.Service.MultiplyBy(5));
-        }
+        ConfigurableInjection.InitializeContainer(builder.Build());
     }
 
-    [Configurable]
-    public class Entity
+    [Test]
+    public void AutofacServiceIsInjected()
     {
-        int value;
+        var entity = new Entity(5);
 
-        public IService Service { get; set; }
+        Assert.IsNotNull(entity.Service);
+        Assert.IsInstanceOf(typeof(Service), entity.Service);
 
-        public Entity(int value)
-        {
-            this.value = value;
-        }
+        Assert.AreEqual(25, entity.Service.MultiplyBy(5));
     }
-
-    public interface IService
-    {
-        int MultiplyBy(int value);
-    }
-
-    public class Service : IService
-    {
-        int _injectedValue;
-
-        public Service(int injectedValue)
-        {
-            _injectedValue = injectedValue;
-        }
-
-        public int MultiplyBy(int value)
-        {
-            return value * _injectedValue;
-        }
-    }
-
 }
+
+[Configurable]
+public class Entity
+{
+    int value;
+
+    public IService Service { get; set; }
+
+    public Entity(int value)
+    {
+        this.value = value;
+    }
+}
+
+public interface IService
+{
+    int MultiplyBy(int value);
+}
+
+public class Service : IService
+{
+    int injectedValue;
+
+    public Service(int injectedValue)
+    {
+        this.injectedValue = injectedValue;
+    }
+
+    public int MultiplyBy(int value)
+    {
+        return value * injectedValue;
+    }
+}
+
