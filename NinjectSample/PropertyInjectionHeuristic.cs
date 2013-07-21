e31a766f9aef25fbdfaf824294093f147323749c
Simon Cropp <simon.cropp@gmail.com>
2013-07-21 12:43:04 +1000

    add more samples

---
+++ NinjectSample/PropertyInjectionHeuristic.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using Ninject;
+using Ninject.Components;
+using Ninject.Selection.Heuristics;
+
+namespace NinjectFodySample
+{
+    /// <summary>
+    /// This class is NOT required by Ninject.Fody, this is only used to avoid using [Inject] attributes
+    /// </summary>
+    public class PropertyInjectionHeuristic : NinjectComponent, IInjectionHeuristic
+    {
+        private readonly IKernel kernel;
+
+        public PropertyInjectionHeuristic(IKernel kernel)
+        {
+            this.kernel = kernel;
+        }
+
+        public bool ShouldInject(MemberInfo member)
+        {
+            var propertyInfo = member.ReflectedType.GetProperty(member.Name);
+
+            if (propertyInfo != null && propertyInfo.CanWrite)
+            {
+                object service = kernel.TryGet(propertyInfo.PropertyType);
+
+                return service != null;
+            }
+
+            return false;
+        }
+    }
+}
