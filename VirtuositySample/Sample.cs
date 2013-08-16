30f409dd1dd2541b5883903f0c26c933a6c6378f
Simon Cropp <simon.cropp@gmail.com>
2013-08-16 11:37:51 +1000

    fix virtuosity unit test

--- VirtuositySample/Sample.cs
+++ VirtuositySample/Sample.cs
@@ -6,7 +6,7 @@ public class VirtuositySample
     [Test]
     public void Run()
     {
-        Assert.IsNotNull(GetType().GetMethod("Method").IsVirtual);
+        Assert.IsTrue(GetType().GetMethod("Method").IsVirtual);
     }
 
     public void Method()
