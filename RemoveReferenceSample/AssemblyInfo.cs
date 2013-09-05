fadf5e0c00a6e3eb1ad93969db8eeaed0e4bc831
Rami Abughazaleh <rami.abughazaleh@gmail.com>
2013-09-05 00:17:58 -0700

    added RemoveReferenceSample BUT produces the following error when trying to run the test: Test 'M:RemoveReferenceSample.Sample.Run' failed: Could not load type 'RemoveReference.RemoveReferenceAttribute' from assembly 'nunit.framework, Version=2.6.2.12296, Culture=neutral, PublicKeyToken=96d09a1eb7f44a77'.

---
+++ RemoveReferenceSample/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using RemoveReference;
+
+[assembly: AssemblyTitle("RemoveReferenceSample")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("RemoveReferenceSample")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: RemoveReference("System.Xml.Linq, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e")]
