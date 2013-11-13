60b581eb3ff39d302a54b0137692e133a1319f5d
Damian Reeves <DamReev@gmail.com>
2013-11-12 22:14:20 -0500

    Add Commander Sample

---
+++ CommanderSample/SampleViewModel.cs
@@ -0,0 +1,48 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Commander;
+
+namespace CommanderSample
+{
+    public class SampleViewModel : INotifyPropertyChanged
+    {
+        private int _ticks;
+
+        public int Ticks
+        {
+            get { return _ticks; }
+            set
+            {
+                if (_ticks == value)
+                {
+                    return;
+                }
+                _ticks = value;
+                OnPropertyChanged();
+            }
+        }
+
+        [OnCommand("TickCommand")]
+        public void OnTick()
+        {
+            Ticks++;
+        }
+
+        [OnCommandCanExecute("TickCommand")]
+        public bool CanTick()
+        {
+            return Ticks < 10;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
