e3c55fcf6cdf734459fa719a3b0646db9791e9b3
Justin Thirkell <justin.thirkell@xero.com>
2013-11-18 21:37:09 +1300

    add EmptyStringGuardSample project

---
+++ EmptyStringGuardSample/Sample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+[TestFixture]
+public class EmptyStringGuardSample
+{
+    [Test]
+    public void Run()
+    {
+        var targetClass = new TargetClass();
+        Assert.Throws<ArgumentException>(() => targetClass.Method(string.Empty));
+    }
+}
+public class TargetClass
+{
+    public void Method(string param)
+    {
+        
+    }
+}
