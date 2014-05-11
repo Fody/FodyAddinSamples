6d943f400194e7091da322419ceb9b1a47a17ea3
Simon Cropp <simon.cropp@gmail.com>
2014-05-11 18:44:02 +1000

    simplify splat test

--- AnotarSplatSample/SplatExceptionSample.cs
+++ AnotarSplatSample/SplatExceptionSample.cs
@@ -18,7 +18,6 @@ public class SplatExceptionSample
 
         var lastMessage = LogCaptureBuilder.LastMessage.Replace(Environment.NewLine, "");
         Assert.AreEqual(@"SplatExceptionSample: Exception occurred in 'Void MyMethod()'. : System.Exception: Foo   at SplatExceptionSample.MyMethod()", lastMessage.Substring(0, lastMessage.LastIndexOf(" in ")));
-        Assert.IsTrue(lastMessage.EndsWith(@"FodyAddinSamples\AnotarSplatSample\SplatExceptionSample.cs:line 27"));
     }
 
     [LogToDebugOnException]
