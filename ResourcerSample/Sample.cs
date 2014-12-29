7083c6ce868e0a5b0b2706368023ba0d9a1dada9
Simon Cropp <simon.cropp@gmail.com>
2014-12-29 11:22:48 +1100

    refs

--- ResourcerSample/Sample.cs
+++ ResourcerSample/Sample.cs
@@ -7,7 +7,9 @@ public class ResourcerSample
     [Test]
     public void Run()
     {
-        var fromResource = Resource.AsString("SampleResource.txt");
+        var fromResource = Resource.AsString("SampleResource2.txt");
+        Assert.AreEqual("Hello", fromResource);
+        var fromResource2 = Resource.AsString("SampleResource4.txt");
         Assert.AreEqual("Hello", fromResource);
     }
 }
