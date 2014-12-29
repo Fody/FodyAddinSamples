cdf618966a97541a37ab42433815c850f4ed120b
Simon Cropp <simon.cropp@gmail.com>
2014-12-29 11:26:21 +1100

    fix error

--- ResourcerSample/Sample.cs
+++ ResourcerSample/Sample.cs
@@ -7,9 +7,7 @@ public class ResourcerSample
     [Test]
     public void Run()
     {
-        var fromResource = Resource.AsString("SampleResource2.txt");
-        Assert.AreEqual("Hello", fromResource);
-        var fromResource2 = Resource.AsString("SampleResource4.txt");
+        var fromResource = Resource.AsString("SampleResource.txt");
         Assert.AreEqual("Hello", fromResource);
     }
 }
