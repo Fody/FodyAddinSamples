30aa1039a534b950e7393456d5c516e0eaf2a183
Simon Cropp <simon.cropp@gmail.com>
2015-02-15 11:19:52 +1100

    add extra sample

---
+++ ConfigureAwaitSample/ConfigureAwaitSample.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Fody;
+using NUnit.Framework;
+
+[TestFixture]
+[ConfigureAwait(false)]
+public class ConfigureAwaitSample
+{
+    [Test]
+    public async void Run()
+    {
+        var beforeAwaitId = Thread.CurrentThread.ManagedThreadId;
+        await Task.Delay(30);
+        Assert.AreNotEqual(Thread.CurrentThread.ManagedThreadId,beforeAwaitId);
+    }
+}
+
