a433e8f67753b09d8d7ef4867ca9ef4bb7413c0d
csnemes <csaba.nemes@outlook.com>
2015-06-06 21:19:34 +0200

    Added examples for tracer.fody

---
+++ TracerOwnLogAdapterSample/Example.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TracerAttributes;
+using TracerOwnLogAdapter;
+using TracerOwnLogAdapter.Adapters;
+
+namespace TracerOwnLogAdapterSample
+{
+    [TestFixture]
+    [NoTrace]
+    public class Example
+    {
+        [Test]
+        public void Test()
+        {
+            new ClassToLog().Run();
+
+            LoggerAdapter.LoggedLines.ForEach(line => Debug.Print(line));
+            Assert.AreEqual(5, LoggerAdapter.LoggedLines.Count);
+        }
+    }
+
+    [TraceOn(TraceTarget.Protected)]
+    public class ClassToLog
+    {
+        public void Run()
+        {
+            MyLog.SomethingImportant(42, "LogInfoLine");
+            int result = 0;
+            WillBeTracedAsProtected(ref result, 100);
+        }
+
+        protected int WillBeTracedAsProtected(ref int result, int input)
+        {
+            WontBeTracedAsPrivate();
+            result = 100 / input;
+            return 42;
+        }
+
+        private void WontBeTracedAsPrivate()
+        {
+
+        }
+    }
+}
