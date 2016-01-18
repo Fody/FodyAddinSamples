59c825e603783c4c2a162dbf61201528acddd478
Kamil Mrzygłód <kmrzyglod@moneygram.com>
2016-01-18 08:45:54 +0100

    Added SealFody sample

---
+++ SealSample/SealSample.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Seal.Fody;
+
+namespace SealSample
+{
+    [TestFixture]
+    public class SealSample
+    {
+        [Test]
+        public void Run()
+        {
+            Assert.IsTrue(typeof(Sealed).IsSealed);
+            Assert.IsFalse(typeof(NonSealed).IsSealed);
+        }
+    }
+
+    class Sealed
+    {
+    }
+
+    [LeaveUnsealed]
+    class NonSealed
+    {
+    }
+}
