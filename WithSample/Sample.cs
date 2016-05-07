5e4a3e18d738df56644f7663c0a7a1b9a4dfafa4
Mikhail Shilkov <shilkov@gmail.com>
2016-05-07 15:50:47 +0200

    Add a sample for With.Fody plugin

---
+++ WithSample/Sample.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class WithSample
+{
+    [Test]
+    public void Run()
+    {
+        var target = new Target(123, "Hello");
+        Assert.AreEqual(456, target.With(456).IntValue);
+        Assert.AreEqual("World", target.With("World").StringValue);
+    }
+}
+
+public class Target
+{
+    public Target(int intValue, string stringValue)
+    {
+        this.IntValue = intValue;
+        this.StringValue = stringValue;
+    }
+
+    public int IntValue { get; }
+
+    public string StringValue { get; }
+
+    // Needed for IntelliSense/Resharper support
+    public Target With<T>(T value) => this;
+}
