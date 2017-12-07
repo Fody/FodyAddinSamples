0a14ae650972b620b9a09163dd60a75e23ff7ec6
Simon Cropp <simon.cropp@gmail.com>
2017-12-07 13:28:59 +1100

    refs

--- ValidarSample/ValidationFactory.cs
+++ ValidarSample/ValidationFactory.cs
@@ -10,9 +10,10 @@ public static class ValidationFactory
     {
         if (!validators.TryGetValue(modelType.TypeHandle, out var validator))
         {
-            var typeName = modelType.Name + "Validator";
-            var type = modelType.Assembly.GetType(modelType.Namespace + "." + typeName, true);
+            var type = modelType.Assembly
+                .GetType($"{modelType.Namespace}.{modelType.Name}Validator", true);
             validator = (IValidator)Activator.CreateInstance(type);
+            validators[modelType.TypeHandle] = validator;
         }
 
         return validator;
