109132edfc197f6d8d7b9294353c18a76f37e476
Simon Cropp <simon.cropp@gmail.com>
2018-02-03 16:47:02 +1100

    .

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-dotnet restore && dotnet build /p:Configuration=ReleaseNetCore /p:Platform="Any CPU"
+dotnet restore && dotnet build /p:Configuration=ReleaseNetCore /p:Platform="Any CPU" /f:netcoreapp2.0
