ed4e232efddd4dc14b7653c7898be243ac3bd767
Simon Cropp <simon.cropp@gmail.com>
2018-04-08 10:19:56 +1000

    refs

---
+++ ScalpelSample/SampleTestFixture.cs
@@ -0,0 +1,11 @@
+﻿using Xunit;
+
+//Have a look at the resultant assembly in release mode. 
+//This class and the nunit reference will have been removed.
+public class SampleTestFixture
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
