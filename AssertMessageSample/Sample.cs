17e4ad453b46924660498535e53b7e06231b8052
Simon Cropp <simon.cropp@gmail.com>
2018-05-13 20:07:34 +1000

    refs

---
+++ AssertMessageSample/Sample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+
+public class AssertMessageSample
+{
+    public AssertMessageSample(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
+
+    ITestOutputHelper output;
+
+    [Fact]
+    public void Run()
+    {
+        var string1 = "samplestring";
+        var string2 = "SampleString";
+
+        try
+        {
+            Assert.True(string2 == string1);
+        }
+        catch (Exception e)
+        {
+            output.WriteLine(e.Message);
+        }
+    }
+}
