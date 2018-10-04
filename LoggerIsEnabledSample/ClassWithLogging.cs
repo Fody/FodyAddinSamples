e646cd514dd3778af961846459e441c9287db82e
Wazowski <jorisdebock@outlook.com>
2018-10-04 12:19:54 +0200

    Add sample for LoggerIsEnabled

---
+++ LoggerIsEnabledSample/ClassWithLogging.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace LoggerIsEnabledSample
+{
+
+    public class ClassWithLogging
+    {
+        private readonly ILogger<ClassWithLogging> _logger;
+
+        public ClassWithLogging(ILogger<ClassWithLogging> logger)
+        {
+            _logger = logger;
+        }
+
+        public void LogTrace()
+        {
+            _logger.LogTrace("message");
+        }
+    }
+}
