b880706e0500e4f3de6e42322027db4de7a47ed3
simoncropp <simon.cropp@gmail.com>
2019-01-02 21:03:45 +1100

    .

--- Samples/JanitorSample.cs
+++ Samples/JanitorSample.cs
@@ -26,4 +26,18 @@ public class JanitorSample
             //must be empty
         }
     }
+
+    /// <summary>
+    /// A IDisposable class we will use to verify that fields in the sample have been disposed
+    /// </summary>
+    [Janitor.SkipWeaving]
+    public class DisposeTracker : IDisposable
+    {
+        public static bool HasDisposedBeenCalled;
+
+        public void Dispose()
+        {
+            HasDisposedBeenCalled = true;
+        }
+    }
 }
