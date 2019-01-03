8e939c82f300d0c25cab4cdcb56873f87f7dfd53
simoncropp <simon.cropp@gmail.com>
2019-01-03 13:26:15 +1100

    .

---
+++ Samples/AnotarLibLogSample/LibLogExceptionSample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Anotar.LibLog;
+using Xunit;
+
+namespace AnotarLibLogSample
+{
+    public class LibLogExceptionSample
+    {
+        [Fact]
+        public void Run()
+        {
+            try
+            {
+                MyMethod();
+            }
+            catch
+            {
+            }
+
+            Assert.Equal("Exception occurred in 'Void MyMethod()'. ", CustomProvider.LastMessage);
+            Assert.NotNull(CustomProvider.LastException);
+        }
+
+        [LogToDebugOnException]
+        static void MyMethod()
+        {
+            throw new Exception("Foo");
+        }
+    }
+}
