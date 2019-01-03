28663ae742b95988809df746a650be23e0a98980
simoncropp <simon.cropp@gmail.com>
2019-01-03 21:53:56 +1100

    merge samples

---
+++ Samples/ToStringSample.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using Xunit;
+
+public class ToStringSample
+{
+    [Fact]
+    public void Run()
+    {
+        var target = new Person
+        {
+            GivenNames = "John",
+            FamilyName = "Smith"
+
+        };
+        Debug.WriteLine(target.ToString());
+        Assert.Equal("{T: \"Person\", GivenNames: \"John\", FamilyName: \"Smith\"}", target.ToString());
+    }
+
+    [ToString]
+    class Person
+    {
+        public string GivenNames { get; set; }
+        public string FamilyName { get; set; }
+
+        [IgnoreDuringToString]
+        public string FullName => $"{GivenNames} {FamilyName}";
+    }
+}
