f1115310e658005b969d2028cb1801d03f19758d
simoncropp <simon.cropp@gmail.com>
2019-01-04 08:08:56 +1100

    cleanup

--- AutoPropertiesSample/AutoPropertiesSample.cs
+++ AutoPropertiesSample/AutoPropertiesSample.cs
@@ -1,18 +1,17 @@
-﻿using System;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using AutoProperties;
 using Xunit;
 // ReSharper disable UnusedParameter.Local
 
-public class AutoPropertiesSetBackingField : INotifyPropertyChanged
+public class AutoPropertiesSample : INotifyPropertyChanged
 {
     int numberOfPropertyChangedCalls;
 
     public string AutoProperty1 { get; set; }
     public string AutoProperty2 { get; set; }
 
-    public AutoPropertiesSetBackingField()
+    public AutoPropertiesSample()
     {
         AutoProperty2.SetBackingField("42");
     }
@@ -43,40 +42,3 @@ public class AutoPropertiesSetBackingField : INotifyPropertyChanged
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
-
-public class AutoPropertiesInterceptor
-{
-    [Fact]
-    public void Run()
-    {
-        Assert.Equal(10, Property1);
-        Assert.Equal("11", Property2);
-
-        Property1 = 42;
-
-        Assert.Equal(45, Property1);
-        Assert.Equal("11", Property2);
-
-        Property2 = "44";
-
-        Assert.Equal(45, Property1);
-        Assert.Equal("47", Property2);
-    }
-
-    [GetInterceptor]
-    T GetInterceptor<T>(string propertyName, T fieldValue)
-    {
-        return (T)Convert.ChangeType(Convert.ToInt32(fieldValue) + 1, typeof(T));
-    }
-
-    [SetInterceptor]
-    void SetInterceptor<T>(T value, string propertyName, out T field)
-    {
-        field = (T)Convert.ChangeType(Convert.ToInt32(value) + 2, typeof(T));
-    }
-
-    public int Property1 { get; set; } = 7;
-
-    public string Property2 { get; set; } = "8";
-}
-
