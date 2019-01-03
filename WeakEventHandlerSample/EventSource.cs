c7a12f603a907bd2812f77c8b3361e0fd37d5d07
simoncropp <simon.cropp@gmail.com>
2019-01-03 22:07:58 +1100

    merge samples

---
+++ WeakEventHandlerSample/EventSource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WeakEventHandlerSample
+{
+    public class EventSource
+    {
+        public event EventHandler<EventArgs> Event;
+
+        public void OnEvent()
+        {
+            Event?.Invoke(this, EventArgs.Empty);
+        }
+
+        public bool HasEventHandlersAttached => Event != null;
+    }
+}
--- WeakEventHandlerSample/Test.cs
+++ WeakEventHandlerSample/Test.cs
@@ -4,46 +4,6 @@ namespace WeakEventHandlerSample
 {
     using Xunit;
 
-    public class EventSource
-    {
-        public event EventHandler<EventArgs> Event;
-
-        public void OnEvent()
-        {
-            Event?.Invoke(this, EventArgs.Empty);
-        }
-
-        public bool HasEventHandlersAttached => Event != null;
-    }
-
-    public class EventSink
-    {
-        readonly EventSource _source;
-        readonly Action<string> _eventTracer;
-
-        public EventSink(EventSource source, Action<string> eventTracer)
-        {
-            _source = source;
-            _eventTracer = eventTracer;
-        }
-
-        public void Subscribe()
-        {
-            _source.Event += Source_Event;
-        }
-
-        public void Unsubscribe()
-        {
-            _source.Event -= Source_Event;
-        }
-
-        [WeakEventHandler.MakeWeak]
-        void Source_Event(object sender, EventArgs e)
-        {
-            _eventTracer("Event");
-        }
-    }
-
     public class Test
     {
         [Fact]
