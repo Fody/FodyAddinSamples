b2496299a8e71119e24be8b536e690ed02e5011d
Chris Copeland <christopher.copeland@radiuspaymentsolutions.com>
2019-01-29 09:36:43 +0000

    Added the Mimick sample code

---
+++ Samples/MimickSample/SampleModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mimick;
+
+namespace MimickSample
+{
+    /// <summary>
+    /// A model class which introduces the property-changed interface.
+    /// </summary>
+    [PropertyChanged]
+    public class SampleModel
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the identity.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tag.
+        /// </summary>
+        [IgnoreChange]
+        public object Tag { get; set; }
+
+        #endregion
+    }
+}
