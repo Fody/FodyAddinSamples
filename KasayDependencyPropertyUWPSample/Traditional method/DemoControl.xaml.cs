c506aea4c15f420da66a9843355741ff7e7faa00
robinzevallos <zevallosrobin@gmail.com>
2019-02-01 17:30:08 -0500

    Added KasayDependencyPropertyUWPSample

---
+++ KasayDependencyPropertyUWPSample/Traditional method/DemoControl.xaml.cs
@@ -0,0 +1,69 @@
+﻿namespace Sample.Traditional
+{
+    using System;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Media;
+
+    public sealed partial class DemoControl : UserControl
+    {
+        public static readonly DependencyProperty BackgroundCircleProperty
+            = DependencyProperty.Register(
+                "BackgroundCircle",
+                typeof(Brush),
+                typeof(DemoControl),
+                null);
+
+        public Brush BackgroundCircle
+        {
+            get => (Brush)GetValue(BackgroundCircleProperty);
+            set => SetValue(BackgroundCircleProperty, value);
+        }
+
+        public static readonly DependencyProperty TextCircleProperty
+            = DependencyProperty.Register(
+                "TextCircle",
+                typeof(String),
+                typeof(DemoControl),
+                null);
+
+        public String TextCircle
+        {
+            get => (String)GetValue(TextCircleProperty);
+            set => SetValue(TextCircleProperty, value);
+        }
+
+        public static readonly DependencyProperty TitleProperty
+            = DependencyProperty.Register(
+                "Title",
+                typeof(String),
+                typeof(DemoControl),
+                null);
+
+        public String Title
+        {
+            get => (String)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public static readonly DependencyProperty DescriptionProperty
+            = DependencyProperty.Register(
+                "Description",
+                typeof(String),
+                typeof(DemoControl),
+                null);
+
+        public String Description
+        {
+            get => (String)GetValue(DescriptionProperty);
+            set => SetValue(DescriptionProperty, value);
+        }
+
+        public DemoControl()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+    }
+}
---
+++ KasayDependencyPropertyUWPSample/With Kasay.DependencyProperty/DemoControl.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace Sample
+{
+    using Kasay.DependencyProperty;
+    using System;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Media;
+
+    public sealed partial class DemoControl : UserControl
+    {
+        [Bind] public Brush BackgroundCircle { get; set; }
+
+        [Bind] public String TextCircle { get; set; }
+
+        [Bind] public String Title { get; set; }
+
+        [Bind] public String Description { get; set; }
+
+        public DemoControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
