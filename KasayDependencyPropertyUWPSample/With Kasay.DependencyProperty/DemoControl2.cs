b2da54eea18d946597197db53b5bf79f98d86470
robinzevallos <zevallosrobin@gmail.com>
2019-02-02 12:38:26 -0500

    csproj to MSBuild.Sdk.Extras

---
+++ KasayDependencyPropertyUWPSample/With Kasay.DependencyProperty/DemoControl2.cs
@@ -0,0 +1,14 @@
+﻿using Kasay.DependencyProperty;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+public sealed partial class DemoControl2 : UserControl
+{
+    [Bind] public Brush BackgroundCircle { get; set; }
+
+    [Bind] public string TextCircle { get; set; }
+
+    [Bind] public string Title { get; set; }
+
+    [Bind] public string Description { get; set; }
+}
