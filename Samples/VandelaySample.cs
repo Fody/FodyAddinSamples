81d27e640c4e6ba2802ee4354bdd2b6a2b04c8ee
Jason Woods <jwoods@appliedsystems.com>
2019-02-21 11:40:34 -0600

    adding Vandelay sample

---
+++ Samples/VandelaySample.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using Xunit;
+
+[assembly: Vandelay.Exporter(typeof(VandelaySample.IExportable))]
+
+namespace VandelaySample
+{
+    public interface IExportable
+    {
+    }
+
+    public class Exportable : IExportable
+    {
+    }
+
+    public class Foo
+    {
+    }
+
+    public class ExportableWithImport : IExportable
+    {
+        [Import]
+        public Foo MyFoo { get; set; }
+    }
+
+    public static class Importer
+    {
+        public static IEnumerable<IExportable> WithoutExport { get; } =
+            Vandelay.Importer.ImportMany<IExportable>("Samples.dll");
+
+        public static IEnumerable<IExportable> WithExport { get; } =
+            Vandelay.Importer.ImportMany<IExportable>("Samples.dll", new Foo());
+    }
+
+    public class Sample
+    {
+        [Fact]
+        public void WithoutExport()
+        {
+            var imports = Importer.WithoutExport.ToList();
+
+            Assert.NotNull(imports);
+            Assert.NotEmpty(imports);
+            Assert.Single(imports);
+        }
+
+        [Fact]
+        public void WithExport()
+        {
+            var imports = Importer.WithExport.ToList();
+
+            Assert.NotNull(imports);
+            Assert.NotEmpty(imports);
+            Assert.Equal(2, imports.Count);
+
+            var withImport = imports.OfType<ExportableWithImport>().Single();
+            Assert.NotNull(withImport.MyFoo);
+        }
+    }
+}
