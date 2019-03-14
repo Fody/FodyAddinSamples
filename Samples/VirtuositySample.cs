fdfff939d9a1420c7c578346db33c65e6a1bf6d5
simoncropp <simon.cropp@gmail.com>
2019-03-14 16:35:06 +1100

    add VirtuositySample

---
+++ Samples/VirtuositySample.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+public class VirtuositySample
+{
+    [Fact]
+    public void Run()
+    {
+        Assert.True(typeof(Target).GetProperty("Property").GetMethod.IsVirtual);
+    }
+
+    public class Target
+    {
+        public string Property { get; set; }
+    }
+}
