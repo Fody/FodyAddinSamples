bf23ca2a80ff47de17321b9c404d3feefcc26471
Simon Cropp <simon.cropp@gmail.com>
2019-04-27 08:32:03 +1000

    redundant weavers

--- TracerOwnLogAdapter/MyLog.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TracerOwnLogAdapter
-{
-    //Specify your own logging methods here with empty body
-    public static class MyLog
-    {
-        public static void SomethingImportant(int importantValue, string message) {}
-    }
-}
