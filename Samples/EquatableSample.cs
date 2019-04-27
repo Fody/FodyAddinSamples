d8e0b39fda88ca1e3b3a466e7dc757699c708dcc
Simon Cropp <simon.cropp@gmail.com>
2019-04-27 14:17:29 +1000

    re-add Equatable sample

---
+++ Samples/EquatableSample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Equatable;
+using Xunit;
+
+public class EquatableSample
+{
+    [ImplementsEquatable]
+    public class Target
+    {
+        [Equals]
+        public string Property { get; set; }
+    }
+
+    [Fact]
+    public void Run()
+    {
+        var left = new Target
+        {
+            Property = "Test",
+        };
+
+        Assert.True(left is IEquatable<Target>);
+
+        var right = new Target
+        {
+            Property = "Test",
+        };
+
+        Assert.Equal(left, right);
+        Assert.Equal(left.GetHashCode(), right.GetHashCode());
+    }
+}
