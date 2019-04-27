d14d5b3e9c1c321a9847be541fa0159f160b2194
Simon Cropp <simon.cropp@gmail.com>
2019-04-27 14:07:08 +1000

    add ionad sample

---
+++ Samples/InoadSample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using Ionad;
+using Xunit;
+
+public class IonadSample
+{
+    [Fact]
+    public void Run()
+    {
+        DateTimeSubstitute.Current = new DateTime(2000, 1, 1);
+        var time = DateTime.Now;
+        Debug.WriteLine(time);
+    }
+
+    [StaticReplacement(typeof(DateTime))]
+    public static class DateTimeSubstitute
+    {
+        public static DateTime Current;
+
+        public static DateTime Now => Current;
+    }
+}
