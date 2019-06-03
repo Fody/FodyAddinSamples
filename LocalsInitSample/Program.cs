84c7e4cf30447c3576dcbd9689d793422cf893e5
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2019-06-03 21:02:50 +0200

    Add LocalsInit example

---
+++ LocalsInitSample/Program.cs
@@ -0,0 +1,7 @@
+﻿using BenchmarkDotNet.Running;
+
+public static class Program
+{
+	public static void Main()
+		=> BenchmarkRunner.Run<StackallocBenchmark>();
+}
---
+++ LocalsInitSample/StackallocBenchmark.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
+using LocalsInit;
+
+[InProcess]
+public unsafe class StackallocBenchmark
+{
+	const int BufferSize = 4096;
+
+	[Benchmark(Baseline = true)]
+	[LocalsInit(true)]
+	public void LocalsInitTrue()
+	{
+		var ptr = stackalloc byte[BufferSize];
+		Consume(ptr);
+	}
+
+	[Benchmark]
+	[LocalsInit(false)]
+	public void LocalsInitFalse()
+	{
+		var ptr = stackalloc byte[BufferSize];
+		Consume(ptr);
+	}
+
+	[SuppressMessage("ReSharper", "UnusedParameter.Local")]
+	[MethodImpl(MethodImplOptions.NoInlining)]
+	static void Consume(void* _)
+	{
+	}
+}
