831d389cdedb3d9a2f7610b35d68f8ce49a27283
SimonCropp <simon.cropp@gmail.com>
2019-12-10 16:03:39 +1000

    remove liblog

--- ReactiveUISample/ReactiveUISample.cs
+++ ReactiveUISample/ReactiveUISample.cs
@@ -11,16 +11,16 @@ public class ReactiveUiSample
     {
         var target = new ReactiveViewModel();
         var notificationOccured = false;
-        target.WhenAnyValue(m => m.Property).Subscribe(x => notificationOccured=true);
+        target.WhenAnyValue(m => m.Property).Subscribe(x => notificationOccured = true);
         Assert.True(notificationOccured);
     }
 
     public IEnableLogger Foo { get; set; }
 
-    public class ReactiveViewModel : ReactiveObject
+    public class ReactiveViewModel :
+        ReactiveObject
     {
         [Reactive]
         public string Property { get; set; }
     }
-
 }
