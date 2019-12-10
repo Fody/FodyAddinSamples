0ba6b3d465722c77724d51a1351825210885a718
SimonCropp <simon.cropp@gmail.com>
2019-12-10 16:15:45 +1000

    Update InoadSample.cs

--- Samples/InoadSample.cs
+++ Samples/InoadSample.cs
@@ -10,7 +10,7 @@ public class IonadSample
     {
         DateTimeSubstitute.Current = new DateTime(2000, 1, 1);
         var time = DateTime.Now;
-        Debug.WriteLine(time);
+        Trace.WriteLine(time);
     }
 
     [StaticReplacement(typeof(DateTime))]
