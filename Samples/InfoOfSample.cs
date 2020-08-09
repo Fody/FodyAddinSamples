70a1b5ac4ab984ac7ee5ade351b3697f1ca51909
Simon Cropp <simon.cropp@gmail.com>
2020-08-09 10:02:19 +1000

    .

---
+++ Samples/InfoOfSample.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+// ReSharper disable UnusedVariable
+
+public class InfoOfSample
+{
+    [Fact]
+    public void PerfWithInfoOf()
+    {
+        var methodInfo1 = Info.OfMethod("Samples", "InternalClass", "Method");
+        for (var i = 0; i < 10000; i++)
+        {
+            Info.OfMethod("Samples", "InternalClass", "Method");
+        }
+    }
+}
